aa27de331f7149d13be880d3a77644592d36531d
ttasjwi <ttasjwi920@gmail.com>
2022-01-31 17:24:05 +0900

    xml 기반 스프링 빈 설정 해보기
    - xml 기반으로 스프링 빈을 등록하는 경험을 해봤다.
    - xml 문서를 스스로 작성해야했는데 IntelliJ Community 버전 기준으로는 편의성 제공이 되지 않아 수동으로 타이핑해야했는데 고통스러웠다.
    - 어노테이션 기반으로 스프링 빈을 설정하는 것이 얼마나 편한 것인지 꺠달았다. 다시는 xml로 config 작성하지 않을거야!

---
+++ src/test/java/com/ttasjwi/study/HelloSpringBoot/xml/XmlAppContext.java
@@ -0,0 +1,19 @@
+package com.ttasjwi.study.HelloSpringBoot.xml;
+
+import com.ttasjwi.study.HelloSpringBoot.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+
+    @Test
+    @DisplayName("xml 기반으로 스프링빈 설정")
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
