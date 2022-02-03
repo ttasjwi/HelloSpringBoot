e56f99a23e090b53245c64942fa8d6ac9c2ab461
ttasjwi <ttasjwi920@gmail.com>
2022-02-03 16:23:39 +0900

    MyLogger 생성 (웹 스코프 - request 스코프)
    - 웹 환경에서만 동작
    - request 1개당 1개의 빈 생성, 요청이 종료될 때까지 관리
    
    - UUID : 랜덤으로 128비트 값을 16진수로 하여 반환함 (거의 안 겹침. 거의 유니크함)

---
+++ src/main/java/com/ttasjwi/study/HelloSpringBoot/common/MyLogger.java
@@ -0,0 +1,36 @@
+package com.ttasjwi.study.HelloSpringBoot.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+@Component
+@Scope(value = "request") // 빈스코프 : request 스코프(http 요청마다 하나당 생성 및 요청이 끝날 때 소멸)
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.printf("[%s][%s]{%s}%n", this.uuid, this.requestURL, message);
+    }
+
+    @PostConstruct // 빈 생성 후 초기화
+    public void init() {
+        this.uuid = UUID.randomUUID().toString();
+        System.out.printf("[%s][%s]{request scope bean create : %s}%n", this.uuid, this.requestURL, this);
+    }
+
+    @PreDestroy // 빈 소멸 전
+    public void close() {
+        System.out.printf("[%s][%s]{request scope bean close : %s}%n", this.uuid, this.requestURL, this);
+    }
+
+}
