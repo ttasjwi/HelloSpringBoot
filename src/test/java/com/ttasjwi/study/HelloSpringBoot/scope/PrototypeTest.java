c2faac6c856bba3b4feda408e6393b9c205ec02d
ttasjwi <ttasjwi920@gmail.com>
2022-02-03 12:39:32 +0900

    프로토타입 빈의 빈 스코프
    - 클라이언트 측에서 빈을 호출 시, 새로운 인스턴스생성, 초기화, 반환
    - 그 이후의 인스턴스 관리에 대해서는 더 이상 관여하지 않음.
    - 따라서 빈을 조회한 클라이언트 측에서 빈을 관리해야함. 종료 메서드에 대한 호출도 클라이언트 측에서 해야함.

---
+++ src/test/java/com/ttasjwi/study/HelloSpringBoot/scope/PrototypeTest.java
@@ -0,0 +1,44 @@
+package com.ttasjwi.study.HelloSpringBoot.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class); // 스프링 컨테이너 생성, 빈 등록, 초기화
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        prototypeBean1.destroy(); // 수동으로 호출해야한다.
+        prototypeBean2.destroy();
+        ac.close();
+    }
+
+}
